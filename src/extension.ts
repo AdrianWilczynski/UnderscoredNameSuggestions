c10ab24b71487821469d0b68efefcc90bba32ed3
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-03-30 20:45:24 +0100

    It's working now 😄

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,8 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     const nameCompletionProvider = vscode.languages.registerCompletionItemProvider('csharp', {
-        provideCompletionItems(document, position, token, context) {
-            const linePrefix = getLinePrefix(document, position);
-            getCompletions(linePrefix);
-            return null;
+        provideCompletionItems(document, position) {
+            return getCompletionItems(getCompletionsTexts(getLinePrefix(document, position)));
         }
     });
     context.subscriptions.push(nameCompletionProvider);
@@ -17,12 +15,43 @@ function getLinePrefix(document: vscode.TextDocument, position: vscode.Position)
     return document.lineAt(position).text.substr(0, position.character);
 }
 
-function getCompletions(line: string) {
+function getCompletionItems(texts: string[]) {
+    return texts.map(t => new vscode.CompletionItem(t, vscode.CompletionItemKind.Variable));
+}
+
+function getCompletionsTexts(line: string) {
     const type = parse(line);
     if (!type) {
-        return undefined;
+        return [];
     }
 
+    if (type.isArray) {
+        return toCompletionTexts(type.baseName, true);
+    }
+
+    const completions = toCompletionTexts(type.baseName, false);
+
+    if (type.isGeneric && isGenericCollection(type.baseName)) {
+        let arg = parseGenericArgument(type.genericArguments);
+
+        if (arg) {
+            completions.push(...toCompletionTexts(arg, true));
+        }
+    }
+
+    return completions;
+}
+
+function toCompletionTexts(name: string, pluralize: boolean) {
+    return toPartialNames(trimInterfacePrefix(name)).map(n => {
+        let name = toUnderscored(toCamelCased(n));
+
+        if (pluralize) {
+            name = toPluralized(name);
+        }
+
+        return name;
+    });
 }
 
 function parse(line: string) {
@@ -34,31 +63,80 @@ function parse(line: string) {
         return undefined;
     }
 
-    const isGeneric = !!matches[7];
-    let genericArguments: string | undefined;
-    let firstGenericArgument: string | undefined;
-    if (isGeneric) {
-        genericArguments = matches[8];
-        if (genericArguments) {
-            firstGenericArgument = getFirstGenericArgument(genericArguments);
-        }
-    }
-
     return {
         fullType: matches[3],
         baseName: matches[6],
-        genericArguments: genericArguments,
-        firstGenericArgument: firstGenericArgument,
-        isArray: !!matches[9],
-        isGeneric: isGeneric
+        genericArguments: matches[8],
+        isGeneric: !!matches[7],
+        isArray: !!matches[9]
     };
 }
 
-function getFirstGenericArgument(args: string) {
+function parseGenericArgument(args: string) {
     const matches = args.match(/^ *(\w+(<[\w ,.<>[\]]+>)?\.)*(\w+)/);
     if (!matches) {
         return undefined;
     }
 
     return matches[3];
+}
+
+const collectionTypes = ['ArraySegment', 'BlockingCollection', 'ConcurrentBag', 'ConcurrentQueue',
+    'ConcurrentStack', 'IProducerConsumerCollection', 'HashSet', 'ICollection',
+    'IList', 'IReadOnlyCollection', 'IReadOnlyList', 'ISet',
+    'LinkedList', 'List', 'Queue', 'SortedSet',
+    'Stack', 'SynchronizedCollection', 'SynchronizedReadOnlyCollection', 'IImmutableList',
+    'IImmutableQueue', 'IImmutableSet', 'IImmutableStack', 'Builder',
+    'ImmutableArray', 'ImmutableHashSet', 'ImmutableList', 'ImmutableQueue',
+    'ImmutableSortedSet', 'ImmutableStack', 'Collection', 'ReadOnlyCollection',
+    'EnumerableRowCollection', 'TypedTableBase', 'EntitySet', 'ISingleResult',
+    'ITable', 'Table', 'IObjectSet', 'ObjectQuery',
+    'ObjectResult', 'ObjectSet', 'EntityCollection', 'DataServiceQuery',
+    'QueryOperationResponse', 'PrincipalSearchResult', 'PrincipalValueCollection', 'FileSystemEnumerable',
+    'EnumerableQuery', 'IOrderedEnumerable', 'IOrderedQueryable', 'IQueryable',
+    'ParallelQuery', 'HttpHeaderValueCollection', 'Tensor', 'ReadOnlyCollectionBuilder',
+    'ExtensionCollection', 'IExtensionCollection', 'ServiceModelExtensionCollectionElement', 'IMessageFilterTable',
+    'MessageFilterTable', 'MessageQueryTable', 'XPathMessageFilterTable', 'FreezableCollection',
+    'TextElementCollection', 'XmlQuerySequence', 'IEnumerable', 'DbSet'];
+
+function isGenericCollection(name: string) {
+    return collectionTypes.includes(name);
+}
+
+function toPartialNames(name: string) {
+    const partialNames: string[] = [];
+
+    let accumulator = '';
+    for (const word of splitIntoWords(name)) {
+        accumulator += word;
+
+        partialNames.push(accumulator);
+    }
+
+    return partialNames;
+}
+
+function splitIntoWords(name: string) {
+    return name.split(/(?<=[a-zA-Z0-9])(?=[A-Z][a-z])/);
+}
+
+function trimInterfacePrefix(name: string) {
+    return /^I[A-Z]/.test(name) ? name.substring(1) : name;
+}
+
+function toPluralized(name: string) {
+    return name.endsWith('s') ? name : name + 's';
+}
+
+function toUnderscored(name: string) {
+    return name.startsWith('_') ? name : '_' + name;
+}
+
+function toCamelCased(name: string) {
+    const underscore = name.match(/^_*/)![0];
+
+    let rest = name.match(/^_*([^_]*)/)![1];
+    rest = rest.replace(/^[A-Z](?![A-Z])/, rest[0].toLowerCase());
+
+    return underscore + rest;
 }
