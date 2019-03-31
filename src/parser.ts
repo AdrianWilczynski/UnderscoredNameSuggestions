103d546249e87cb3317222ac2e3b384dd28aa230
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-03-31 22:11:49 +0200

    Accept tabs as whitespace characters

--- src/parser.ts
+++ src/parser.ts
@@ -1,35 +1,35 @@
 import { isKeyword } from './constants';
 
 export function parse(line: string) {
-    //                                              modifiers                                            qualifiers          name  generic_arguments  array_brackets                                 
-    const regex = /^ *((public|private|protected|internal|new|volatile|unsafe|static|readonly) +)*((\w+(<[\w ,.<>[\]]+>)?\.)*(\w+)(<([\w ,.<>[\]]+)>)?(\[[ ,]*])*) +$/;
+	//                                              modifiers                                                      qualifiers           name  generic_arguments   array_brackets                                 
+	const regex = /^[ \t]*((public|private|protected|internal|new|volatile|unsafe|static|readonly)[ \t]+)*((\w+(<[\w \t,.<>[\]]+>)?\.)*(\w+)(<([\w \t,.<>[\]]+)>)?(\[[ \t,]*])*)[ \t]+$/;
 
-    const matches = line.match(regex);
-    if (!matches) {
-        return undefined;
-    }
+	const matches = line.match(regex);
+	if (!matches) {
+		return undefined;
+	}
 
-    const baseName = matches[6];
-    if (isKeyword(baseName)) {
-        return undefined;
-    }
+	const baseName = matches[6];
+	if (isKeyword(baseName)) {
+		return undefined;
+	}
 
-    const isGeneric = !!matches[7];
+	const isGeneric = !!matches[7];
 
-    return {
-        fullType: matches[3],
-        baseName: baseName,
-        genericArgument: isGeneric ? parseGenericArgument(matches[8]) : undefined,
-        isGeneric: isGeneric,
-        isArray: !!matches[9]
-    };
+	return {
+		fullType: matches[3],
+		baseName: baseName,
+		genericArgument: isGeneric ? parseGenericArgument(matches[8]) : undefined,
+		isGeneric: isGeneric,
+		isArray: !!matches[9]
+	};
 }
 
 function parseGenericArgument(args: string) {
-    const matches = args.match(/^ *(\w+(<[\w ,.<>[\]]+>)?\.)*(\w+)/);
-    if (!matches) {
-        return undefined;
-    }
+	const matches = args.match(/^[ \t]*(\w+(<[\w \t,.<>[\]]+>)?\.)*(\w+)/);
+	if (!matches) {
+		return undefined;
+	}
 
-    return matches[3];
+	return matches[3];
 }
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,109 +3,118 @@ import { getCompletionsTexts } from '../completionTextProvider';
 
 suite('Extension should', function () {
 
-    test('Be working ^^', function () {
-        const line = 'private ItemRepository ';
+	test('Be working ^^', function () {
+		const line = 'private ItemRepository ';
 
-        let completions = getCompletionsTexts(line);
+		let completions = getCompletionsTexts(line);
 
-        assert.includeMembers(completions, ['_item', '_itemRepository']);
-        assert.lengthOf(completions, 2);
-    });
+		assert.includeMembers(completions, ['_item', '_itemRepository']);
+		assert.lengthOf(completions, 2);
+	});
 
-    test('Parse generic type', function () {
-        const line = 'private readonly ItemRepository<Item> ';
+	test('Parse generic type', function () {
+		const line = 'private readonly ItemRepository<Item> ';
 
-        const completions = getCompletionsTexts(line);
+		const completions = getCompletionsTexts(line);
 
-        assert.includeMembers(completions, ['_item', '_itemRepository']);
-        assert.lengthOf(completions, 2);
-    });
+		assert.includeMembers(completions, ['_item', '_itemRepository']);
+		assert.lengthOf(completions, 2);
+	});
 
-    test('Ignore keywords', function () {
-        const line = 'private readonly ';
+	test('Ignore keywords', function () {
+		const line = 'private readonly ';
 
-        const completions = getCompletionsTexts(line);
+		const completions = getCompletionsTexts(line);
 
-        assert.lengthOf(completions, 0);
-    });
+		assert.lengthOf(completions, 0);
+	});
 
-    test('Provide hints despite multiple trailing spaces', function () {
-        const line = 'private ItemRepository     ';
+	test('Provide hints despite multiple trailing spaces', function () {
+		const line = 'private ItemRepository     ';
 
-        const completions = getCompletionsTexts(line);
+		const completions = getCompletionsTexts(line);
 
-        assert.isTrue(completions.length > 0);
-    });
-
-
-    test('Parse qualified types', function () {
-        const line = 'private Repositories.ItemRepository ';
-
-        const completions = getCompletionsTexts(line);
-
-        assert.includeMembers(completions, ['_item', '_itemRepository']);
-        assert.lengthOf(completions, 2);
-    });
-
-    test('Parse qualified, generic types', function () {
-        const line = 'private Repositories<MyType>.ItemRepository<MyOtherType<string, AnotherType>> ';
-
-        const completions = getCompletionsTexts(line);
-
-        assert.includeMembers(completions, ['_item', '_itemRepository']);
-        assert.lengthOf(completions, 2);
-    });
-
-    test('Provide hints for partial names', function () {
-        const line = 'private MyItemRepository ';
-
-        const completions = getCompletionsTexts(line);
-
-        assert.includeMembers(completions, ['_my', '_myItem', '_myItemRepository']);
-    });
-
-    test('Parse generic collection types and pluralize them', function () {
-        const line = 'private IEnumerable<MyCustomType> ';
-
-        const completions = getCompletionsTexts(line);
-
-        assert.lengthOf(completions, 4);
-        assert.includeMembers(completions, ['_enumerable', '_mys', '_myCustoms', '_myCustomTypes']);
-    });
+		assert.isTrue(completions.length > 0);
+	});
 
 
-    test("Parse array", function () {
-        const line = 'private ItemService[][,] ';
+	test('Parse qualified types', function () {
+		const line = 'private Repositories.ItemRepository ';
 
-        const completions = getCompletionsTexts(line);
+		const completions = getCompletionsTexts(line);
 
-        assert.lengthOf(completions, 2);
-        assert.includeMembers(completions, ['_items', '_itemServices']);
-    });
+		assert.includeMembers(completions, ['_item', '_itemRepository']);
+		assert.lengthOf(completions, 2);
+	});
 
-    test("Parse array of generics", function () {
-        const line = 'private ItemService<IDictionary<string, int>>[][,] ';
+	test('Parse qualified, generic types', function () {
+		const line = 'private Repositories<MyType>.ItemRepository<MyOtherType<string, AnotherType>> ';
 
-        const completions = getCompletionsTexts(line);
+		const completions = getCompletionsTexts(line);
 
-        assert.lengthOf(completions, 2);
-        assert.includeMembers(completions, ['_items', '_itemServices']);
-    });
+		assert.includeMembers(completions, ['_item', '_itemRepository']);
+		assert.lengthOf(completions, 2);
+	});
 
-    test("Ignore this nonsense", function () {
-        assert.lengthOf(getCompletionsTexts('private My<Ty>pe '), 0);
-        assert.lengthOf(getCompletionsTexts('private MyType{} '), 0);
-        assert.lengthOf(getCompletionsTexts('private 7Type254)&^(#)89 '), 0);
-        assert.lengthOf(getCompletionsTexts('private [MyType] '), 0);
-        assert.lengthOf(getCompletionsTexts('private MyType[]] '), 0);
-        assert.lengthOf(getCompletionsTexts('public void MethodName(string value) '), 0);
-        assert.lengthOf(getCompletionsTexts('public void MethodName(string ) '), 0);
-        assert.lengthOf(getCompletionsTexts('public void PropertyName => '), 0);
-        assert.lengthOf(getCompletionsTexts('if (couldItBeTrue == '), 0);
-        assert.lengthOf(getCompletionsTexts('if (value < '), 0);
-        assert.lengthOf(getCompletionsTexts('if (value< '), 0);
-        assert.lengthOf(getCompletionsTexts('public class '), 0);
-        assert.lengthOf(getCompletionsTexts('class '), 0);
-        assert.lengthOf(getCompletionsTexts('namespace '), 0);
-    });
+	test('Provide hints for partial names', function () {
+		const line = 'private MyItemRepository ';
+
+		const completions = getCompletionsTexts(line);
+
+		assert.includeMembers(completions, ['_my', '_myItem', '_myItemRepository']);
+	});
+
+	test('Parse generic collection types and pluralize them', function () {
+		const line = 'private IEnumerable<MyCustomType> ';
+
+		const completions = getCompletionsTexts(line);
+
+		assert.lengthOf(completions, 4);
+		assert.includeMembers(completions, ['_enumerable', '_mys', '_myCustoms', '_myCustomTypes']);
+	});
+
+
+	test("Parse array", function () {
+		const line = 'private ItemService[][,] ';
+
+		const completions = getCompletionsTexts(line);
+
+		assert.lengthOf(completions, 2);
+		assert.includeMembers(completions, ['_items', '_itemServices']);
+	});
+
+	test("Parse array of generics", function () {
+		const line = 'private ItemService<IDictionary<string, int>>[][,] ';
+
+		const completions = getCompletionsTexts(line);
+
+		assert.lengthOf(completions, 2);
+		assert.includeMembers(completions, ['_items', '_itemServices']);
+	});
+
+	test("Accept tabs as whitespace characters", function () {
+		const line = '\tprivate\tItemService<IDictionary<string, \tint>>[][,]\t \t';
+
+		const completions = getCompletionsTexts(line);
+
+		assert.lengthOf(completions, 2);
+		assert.includeMembers(completions, ['_items', '_itemServices']);
+	});
+
+	test("Ignore this nonsense", function () {
+		assert.lengthOf(getCompletionsTexts('private My<Ty>pe '), 0);
+		assert.lengthOf(getCompletionsTexts('private MyType{} '), 0);
+		assert.lengthOf(getCompletionsTexts('private 7Type254)&^(#)89 '), 0);
+		assert.lengthOf(getCompletionsTexts('private [MyType] '), 0);
+		assert.lengthOf(getCompletionsTexts('private MyType[]] '), 0);
+		assert.lengthOf(getCompletionsTexts('public void MethodName(string value) '), 0);
+		assert.lengthOf(getCompletionsTexts('public void MethodName(string ) '), 0);
+		assert.lengthOf(getCompletionsTexts('public void PropertyName => '), 0);
+		assert.lengthOf(getCompletionsTexts('if (couldItBeTrue == '), 0);
+		assert.lengthOf(getCompletionsTexts('if (value < '), 0);
+		assert.lengthOf(getCompletionsTexts('if (value< '), 0);
+		assert.lengthOf(getCompletionsTexts('public class '), 0);
+		assert.lengthOf(getCompletionsTexts('class '), 0);
+		assert.lengthOf(getCompletionsTexts('namespace '), 0);
+	});
 });
